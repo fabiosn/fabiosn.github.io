8c0028b9d17ed6ccc34e2d283e0332ff8e47a0fc
fabio <fabio.queluci@gmail.com>
2017-01-20 14:04:06 -0200

    Add a Gulp task to process JS files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,24 @@
 var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
+var jshint = require('gulp-jshint');
+var minify = require('gulp-minify');
 
 gulp.task('minify-css', function() {
   return gulp.src('src/resources/css/*css')
   .pipe(cleanCSS({ compatibility: 'ie8' }))
   .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('process-js', function() {
+  gulp.src('src/scripts/*.js')
+  .pipe(jshint())
+  .pipe(jshint.reporter('jshint-stylish'))
+  .pipe(minify({
+    ext: {
+      min: '.min.js'
+    }
+  }))
+  .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('move-bower-components', function() {
