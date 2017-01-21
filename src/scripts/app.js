8b7f3d449fcd8c7c03a024709188090377241df4
fabio <fabio.queluci@gmail.com>
2017-01-21 01:37:42 -0200

    Redesign the way projects are shown and fetch more data from the GitHub's API.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,6 @@
 var app = angular.module("fabiosnApp", []);
 
-app.controller('MainController', ['$scope', '$http', function($scope, $http) {
+app.controller('MainController', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {
   $http.get('https://api.github.com/users/fabiosn').then(
     function(response) {
       $scope.user = response.data;
@@ -12,9 +12,18 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http) {
   $http.get('https://api.github.com/users/fabiosn/repos').then(
     function(response) {
       $scope.repos = response.data;
+      $scope.repos.map(function(repo) {
+        $http.get(repo.languages_url).then(
+          function(response) {
+            repo.languages = response.data;
+          }
+        );
+      });
     },
     function(response) {
     }
   );
+
+  $rootScope.keys = Object.keys;
 }]);
 
