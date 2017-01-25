be90f85350fb5fc7429b5eab88fede476fbfbda4
fabio <fabio.queluci@gmail.com>
2017-01-25 14:06:45 -0200

    Write a custom filter: so a case insensitive search on name, description and languages of my repositories.

---
+++ app/filters.js
@@ -0,0 +1,17 @@
+angular.module('fabiosnApp')
+.filter('byName', function() {
+  return function(repos, searchParam) {
+    searchParam = searchParam.toLowerCase();
+
+    return repos.filter(function(repo) {
+      //get the list of languages as a string
+      var languages = Object.keys(repo.languages).join(', ');
+
+      return (
+        (repo.name.toLowerCase().indexOf(searchParam) > -1)
+        || (repo.description.toLowerCase().indexOf(searchParam) > -1)
+        || (languages.toLowerCase().indexOf(searchParam) > -1)
+      );
+    });
+  };
+});
