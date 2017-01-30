9b7347b27e8ddbfc8fb7f1d88a1c52426a819cf7
fabio <fabio.queluci@gmail.com>
2017-01-30 14:27:19 -0200

    Add the basic routes I need

--- app/app.js
+++ app/app.js
@@ -1,1 +1,12 @@
-angular.module("fabiosnApp", []);
+angular.module("fabiosnApp", ['ngRoute'])
+.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+  $locationProvider.hashPrefix('');
+
+  $routeProvider
+  .when('/', {
+    templateUrl: 'app/partials/home.html',
+  })
+  .when('/cv', {
+    templateUrl: 'app/partials/curriculum.html'
+  });
+}]);
